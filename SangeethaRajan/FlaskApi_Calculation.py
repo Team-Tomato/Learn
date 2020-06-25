515bb61e00ab190298cd64d48281a2fed5240f4f
Sangeetha <sangeerajan2k1@gmail.com>
2020-06-25 22:55:20 +0530

    updated the changes

--- SangeethaRajan/FlaskApi_Calculation.py
+++ SangeethaRajan/FlaskApi_Calculation.py
@@ -1,43 +1,61 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
 
-app1 = Flask(__name__)
+app = Flask(__name__)
 
 
-@app1.route('/<int:num1>/<int:num2>/<method>')
-def add(num1, num2, method):
-    if method == 'Add':
-        return jsonify({'Response code': 200, 'Addition': num1+num2})
-    else:
-        return jsonify({'Response code': 400, 'Error': 'Invalid input'})
+@app.route('/')
+def task():
+    return 'flask task'
 
 
-@app1.route('/<int:num1>/<int:num2>/<method>')
-def sub(num1, num2, method):
-    if method == 'Sub':
-        return jsonify({'Response code': 200, 'Subtraction': num1-num2})
-    else:
-        return jsonify({'Response code': 400, 'Error': 'Invalid input'})
+@app.route('/api/v1/question/add')
+def calculation():
+  try:
+     num1 = request.args.get('num1')
+     num2 = request.args.get('num2')
+     result = int(num1)+int(num2)
+     return jsonify({'Response code': 200, 'Addition': result})
+  except:
+      return jsonify({'Response code': 400, 'Error': 'Invalid input'})
 
 
-@app1.route('/<int:num1>/<int:num2>/<method>')
-def mul(num1, num2, method):
-    if method == 'Mul':
-        return jsonify({'Response code': 200, 'Multiplication': num1*num2})
-    else:
-        return jsonify({'Response code': 400, 'Error': 'Invalid input'})
+@app.route('/api/v1/question/sub')
+def calculation():
+  try:
+     num1 = request.args.get('num1')
+     num2 = request.args.get('num2')
+     result = int(num1)-int(num2)
+     return jsonify({'Response code': 200, 'Subtraction': result})
+  except:
+      return jsonify({'Response code': 400, 'Error': 'Invalid input'})
 
 
-@app1.route('/<int:num1>/<int:num2>/<method>')
-def div(num1, num2, method):
-    if num2 != 0 and method == 'Div':
-        return jsonify({'Response code': 200, 'Division ': num1/num2})
-    else:
-        return jsonify({'Response code': 400, 'Error': 'Invalid input'})
+@app.route('/api/v1/question/mul')
+def calculation():
+  try:
+     num1 = request.args.get('num1')
+     num2 = request.args.get('num2')
+     result = int(num1)*int(num2)
+     return jsonify({'Response code': 200, 'Multiply': result})
+  except:
+      return jsonify({'Response code': 400, 'Error': 'Invalid input'})
+
+
+@app.route('/api/v1/question/div')
+def calculation():
+  try:
+     num1 = request.args.get('num1')
+     num2 = request.args.get('num2')
+     result = int(num1)/int(num2)
+     return jsonify({'Response code': 200, 'Division': result})
+  except:
+      return jsonify({'Response code':  400, 'Error': 'Invalid input'})
 
 
 if __name__ == '__main__':
-    app1.run(debug=True)
+    app.run(debug=True)
 
 
 
