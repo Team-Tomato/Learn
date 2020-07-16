e88f43c7a6296d6fa5e3f7ea6b68aeb62314b8ca
smartsri18 <sridharan479@gmail.com>
2020-07-16 23:30:23 +0530

    Revert "fetched news api using rest-client gem"
    This reverts commit 6e5dd5c97f3d4d95e0944810687ee02ef5fb1cbe.

--- Poomathi/news-fetch.rb
+++ Poomathi/news-fetch.rb
@@ -1,10 +1,7 @@
-require 'rest-client'
-require 'json'
-response = RestClient.get('http://newsapi.org/v2/top-headlines?country=in&apiKey=0630ee9a73044403bf6d17b49500d47d')
-response = JSON.parse(response)
-keys_to_extract = [ "title", "name", "description", "url" ]
-response["articles"].each do |article|
-  article.select! { |key, value| ( keys_to_extract.include?(key) || ( key == "source" && value.select! { |key, value| keys_to_extract.include?(key) }))}
-  puts article
-  puts "-----------------"
-end
+require 'news-api'
+
+newsapi = News.new("0630ee9a73044403bf6d17b49500d47d")             
+
+top_headlines = newsapi.get_top_headlines(language: 'en',country: 'in')
+
+puts (json: top_headlines)
