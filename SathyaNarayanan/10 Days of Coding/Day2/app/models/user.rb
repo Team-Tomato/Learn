deb12fc685bb87eedc8b3c47445ff9cc8a42b2fa
sathya0803 <sathyastunner0803@gmail.com>
2020-09-09 14:53:40 +0530

    DAY 3

--- SathyaNarayanan/10 Days of Coding/Day2/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-	validates :name,:phone,:email,:city,:pin , presence:true
-	validates :email, format: {with: /.*@.*/ }
-	validates :state , length: { minimum: 2}
-	validates :phone,:pin , numericality: {only_integer:true, message:"letters not allowed "}
-end
--- SathyaNarayanan/10 Days of Coding/Day2/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-	resources :users, only: [:index, :create]
-  get 'users/index', to:'users#index'
-  get 'users/create',to:'users#create'
- end
