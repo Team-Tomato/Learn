3fe4d70114aacc92776b5661b074fd7a3cd09112
Aravind <aravindbalaji006@gmail.com>
2020-09-10 11:48:59 +0530

    LoginForm

--- Aravind/10Daysofcode/login-app/src/App.js
+++ Aravind/10Daysofcode/login-app/src/App.js
@@ -1,40 +1,78 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
+const App = () => {
+  const [mail, setMail] = useState("");
+  const Handlemail = (event) => {
+    setMail(event.currentTarget.value);
+    console.log(mail);
+  };
 
-function App() {
-    return (
-      <div className="container-fluid">
-<div className="row">
-<div className="col"></div>
-<div className="col box">
-<h1>Login Form </h1>
-      <Form>
-      <FormGroup>
-        <Label for="Email">Email</Label>
-        <Input type="email" name="email" className="email" placeholder="Enter your email" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" className="password" placeholder="Enter your password" />
-      </FormGroup>
-      <FormGroup check>
-      <Label check>
-      <Input type="checkbox" />
-      Remember me
+  const [pass, setPass] = useState("");
+  const Handlepass = (event) => {
+    setPass(event.currentTarget.value);
+    console.log(pass);
+  };
 
-      </Label>
-</FormGroup>
-      <Button outline color="success" className="btn-block">Submit</Button>
+  function Clicked() {
+    if (mail === "" && pass === "") {
+      alert("Enter Email and Password");
+    }
+  }
 
-</Form>
-</div>
-<div className="col"></div>
-</div>
-</div>
-    );
-}
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col box">
+          <h1>Login Form </h1>
+          <Form>
+            <FormGroup>
+              <Label for="Email">Email</Label>
+              <Input
+                onChange={Handlemail}
+                type="email"
+                name="email"
+                id="email"
+                className="email"
+                placeholder="Enter your email"
+                class="form-control"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                onChange={Handlepass}
+                type="password"
+                id="pass"
+                name="password"
+                className="password"
+                placeholder="Enter your password"
+                class="form-control"
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" />
+                Remember me
+              </Label>
+            </FormGroup>
+            <Button
+              onClick={Clicked}
+              outline
+              color="success"
+              className="btn-block"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
