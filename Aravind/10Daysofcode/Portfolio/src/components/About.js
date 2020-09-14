b0463e907d6a56fbc26bebb2bae1c0c36efdf467
Aravind <aravindbalaji006@gmail.com>
2020-09-14 12:22:31 +0530

    React Portfolio

---
+++ Aravind/10Daysofcode/Portfolio/src/components/About.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src="images/20200622_204310_0000.jpg"
+              alt=""
+            />
+          </div>
+
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>{resumeData.aboutme}</p>
+
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{resumeData.name}</span>
+                  <br />
+                  <span>{resumeData.address}</span>
+                  <br />
+                  <span>{resumeData.website}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
