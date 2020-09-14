8f6ed21370cc1be4506d4f0516565ddb97600dfd
Subash <subash04012001@gmail.com>
2020-09-14 11:59:04 +0530

    Simple Portfolio --- ReactJS

---
+++ Subash/portfolio/src/resumeData.js
@@ -0,0 +1,65 @@
+let resumeData = {
+  name: "Subash Devarajan",
+  role: "Backend Developer",
+  roleDescription:
+    "I like dabbling in various parts of backend development and like to learn about new technologies, write technical articles or simply watch movies in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "www.linkedin.com/in/subash-devarajan-04168319a",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/SubashDevarajan",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently a 3rd year student at The College of Engineering Guindy and pursuing my Msc IT from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  education: [
+    {
+      UniversityName: "Anna University",
+      specialization: "M.Sc Information Technology (5 Years)",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2023",
+    },
+    {
+      UniversityName: "DAV Matriculation Higher Secondary School",
+      specialization: "HSC",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2018",
+    },
+    {
+      UniversityName: "Sunshine Academy Matric. Hr. Sec. School",
+      specialization: "SSLC",
+      MonthOfPassing: "Mar",
+      YearOfPassing: "2016",
+    },
+  ],
+  skills: [
+    {
+      skillname: "HTML5/JS/CSS",
+    },
+    {
+      skillname: "Python",
+    },
+    {
+      skillname: "DSA",
+    },
+    {
+      skillname: "Google Scripts",
+    },
+    {
+      skillname: "Java",
+    },
+  ],
+};
+
+export default resumeData;
